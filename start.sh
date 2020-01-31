d4ad83f13a5d50a169cb0857e7330a46319d9c51
Matthew Mattox <matthew.mattox@rancher.com>
2020-01-30 20:45:22 -0600

    mmattox

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-export AWS_ACCESS_KEY_ID=$KEY
-export AWS_SECRET_ACCESS_KEY=$SECRET
-export AWS_DEFAULT_REGION=$REGION
+export AWS_ACCESS_KEY_ID=$s3AccessKey
+export AWS_SECRET_ACCESS_KEY=$s3SecretKey
+export AWS_DEFAULT_REGION=$S3_REGION
 export CLUSTER=$CLUSTER
 
 echo "Creating AWS creds..."
@@ -16,13 +16,8 @@ echo '[default]' > ~/.aws/config
 echo 'region='"$REGION" >> ~/.aws/config
 echo 'output=json' >> ~/.aws/config
 
-#echo "Creating kubeconfig..."
-#mkdir -p ~/.kube/
-#kubectl get secret -n kube-system kube-admin -o jsonpath={.data.Config} | base64 -d > ~/.kube/config
-#sed -i -e 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/127.0.0.1/g' ~/.kube/config
-
 #echo "Running once during startup..."
-#/sync.sh
+/sync.sh
 
 echo "Setting up cron..."
 echo "$CRON_SCHEDULE /sync.sh" >> /var/spool/cron/crontabs/root
