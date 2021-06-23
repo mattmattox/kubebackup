7386c759900cf22baf76603b8ff523dbb8a63977
Matthew Mattox <matt.mattox@suse.com>
2021-06-23 00:02:25 -0500

    Testing helm deployment

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd /drone/src/
+
+DRONE_TAG=$1
+
+if [[ -z $DRONE_TAG ]]
+then
+  echo "Missing DRONE_TAG"
+  exit 1
+fi
+
+echo "Find and replace DRONE_TAG..."
+sed -i "s/DRONE_TAG/${DRONE_TAG}" ./Chart/Chart.yaml
+sed -i "s/DRONE_TAG/${DRONE_TAG}" ./Chart/values.yaml
+
+echo "Packaging helm chart..."
+helm package ./Chart/ --version $DRONE_TAG --app-version $DRONE_TAG
+
+echo "Pulling down chart repo..."
+mkdir -p helm-repo
+cd helm-repo
+git clone git@github.com:SupportTools/helm-chart.git .
+
+echo "Copying package into repo..."
+cp /drone/src/kubebackup-*.tgz .
+
+echo "Reindexing repo..."
+helm repo index --url https://charts.support.tools/ --merge index.yaml .
+
+echo "Publishing to Chart repo..."
+git add .
+git commit -m "Publishing KubeBackup ${DRONE_TAG}"
+git push
