578e676aa7ec8c58e476c0a057f593eb17cff6e7
Matthew Mattox <matt.mattox@suse.com>
2021-06-23 01:07:20 -0500

    Testing helm charts with Dev and Prod

--- build.sh
+++ build.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
 
+help() {
+
+  echo "Build Script
+  Usage: build.sh -t DRONE_TAG -b DRONE_BUILD_NUMBER -e ENV
+  All flags are optional
+  -t    Drone Tag (v1.2.3)
+  -b    Drone build number (10)
+  -e    Environment (dev|production)"
+
+}
+
+while getopts ":d:s:r:fph" opt; do
+  case $opt in
+    t)
+      DRONE_TAG="${OPTARG}"
+      ;;
+    b)
+      DRONE_BUILD_NUMBER="${OPTARG}"
+      ;;
+    b)
+      ENV="${OPTARG}"
+      ;;
+    h)
+      help && exit 0
+      ;;
+    :)
+      techo "Option -$OPTARG requires an argument."
+      exit 1
+      ;;
+    *)
+      help && exit 0
+  esac
+done
+
 cd /drone/src/
 
-DRONE_TAG=$1
-
-if [[ -z $DRONE_TAG ]]
+if [[ -z $DRONE_TAG ]] || [[ -z $DRONE_BUILD_NUMBER ]] || [[ -z $ENV]]
 then
-  echo "Missing DRONE_TAG"
+  help
   exit 1
 fi
 
@@ -22,13 +54,30 @@ helm package ./Chart/ --version $DRONE_TAG --app-version $DRONE_BUILD_NUMBER
 echo "Pulling down chart repo..."
 mkdir -p helm-repo
 cd helm-repo
-git clone git@github.com:SupportTools/helm-chart.git .
+if [[ ${ENV} == "production" ]]
+then
+  git clone git@github.com:SupportTools/helm-chart.git .
+elif [[ ${ENV} == "dev" ]]
+then
+  git clone git@github.com:SupportTools/helm-chart-dev.git .
+else
+  echo "Unknown Environment"
+fi
 
 echo "Copying package into repo..."
 cp /drone/src/kubebackup-*.tgz .
 
 echo "Reindexing repo..."
-helm repo index --url https://charts.support.tools/ --merge index.yaml .
+if [[ ${ENV} == "production" ]]
+then
+  helm repo index --url https://charts.support.tools/ --merge index.yaml .
+elif [[ ${ENV} == "dev" ]]
+then
+  helm repo index --url https://charts-dev.support.tools/ --merge index.yaml .
+else
+  echo "Unknown Environment"
+fi
+
 
 echo "Publishing to Chart repo..."
 git add .
